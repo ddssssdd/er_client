79ba365b26a96815847b2530b82f88b1d7264c63
fuxinyu@gmail.com <fuxinyu@gmail.com>
2013-06-25 09:32:27 +0800

    Initial Commit

---
+++ ExpenseReport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpenseReport
+//
+//  Created by Fu Steven on 6/25/13.
+//  Copyright (c) 2013 Fu Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ ExpenseReport/FirstViewController.m
@@ -0,0 +1,39 @@
+//
+//  FirstViewController.m
+//  ExpenseReport
+//
+//  Created by Fu Steven on 6/25/13.
+//  Copyright (c) 2013 Fu Steven. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"First", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
